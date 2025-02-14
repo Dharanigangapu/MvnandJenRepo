6a44419ff003946a86b1bd8d9065835aaa91d4fe
Dharanigangapu <dharaniganga1308@gmail.com>
2025-02-14 11:23:12 +0530

    add

---
+++ SeleniumPracticeMavenProject/src/test/java/pac_02/contactTest.java
@@ -0,0 +1,29 @@
+package pac_02;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+	
+	@Test
+	public void createcontact()
+	{
+		String URL = System.getProperty("url");
+		String BROWSER=System.getProperty("browse");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		
+	System.out.println("execute ");
+	}
+	@Test
+	public void modifythecontact()
+	{
+	System.out.println("execute ");
+	}
+	
+
+}
---
+++ SeleniumPracticeMavenProject/src/test/java/pac_1/orgTest.java
@@ -0,0 +1,19 @@
+package pac_1;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	
+	@Test
+	public void createorg()
+	{
+	System.out.println("execute ");
+	}
+	@Test
+	public void modifytheorg()
+	{
+	System.out.println("execute ");
+	}
+	
+
+}
